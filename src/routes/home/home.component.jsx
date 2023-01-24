ff207bc9609e67c740f049ebd89c2ad9e0afad47
JamesdsWarner <jamesdswarner@gmail.com>
2023-01-24 17:31:22 +0000

    updating front page text

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,11 +1,11 @@
-import * as Styled from "./home.styles";
-import Button from "../../components/button/button.component";
-import Banner from "../../components/banner/banner.component";
-import VideoCatalogue from "../../components/video-catalogue/video-catalogue.component";
-import { Link } from "react-router-dom";
-import Skills from "../../components/skills/skills.component";
-import WebpageCatalogue from "../../components/webpage-catalogue/webpage-catalogue.component";
-import Typography from "../../components/Typography/typography.component";
+import * as Styled from './home.styles';
+import Button from '../../components/button/button.component';
+import Banner from '../../components/banner/banner.component';
+import VideoCatalogue from '../../components/video-catalogue/video-catalogue.component';
+import { Link } from 'react-router-dom';
+import Skills from '../../components/skills/skills.component';
+import WebpageCatalogue from '../../components/webpage-catalogue/webpage-catalogue.component';
+import Typography from '../../components/Typography/typography.component';
 
 const Home = () => {
   return (
@@ -17,7 +17,7 @@ const Home = () => {
               <Styled.IntroductionContainer>
                 <Styled.TitleContainer>
                   <Typography type="heading">I'm James Warner.</Typography>
-                  <Typography type="second-heading">Video Editor/Web Developer</Typography>
+                  <Typography type="second-heading">Web Developer/Video Editor</Typography>
                 </Styled.TitleContainer>
                 <Styled.MainImageContainer>
                   <Styled.MainImageBackground alt="Profile image background" />
@@ -33,7 +33,7 @@ const Home = () => {
                   graphics and sound design.
                 </Typography>
                 <Styled.ButtonsContainer>
-                  {" "}
+                  {' '}
                   <Link to="/contact">
                     <Button buttonType="contact-large">Contact me</Button>
                   </Link>
@@ -44,7 +44,7 @@ const Home = () => {
               <WebpageCatalogue />
               <Styled.LineBreak />
               <Typography size="large" type="fancy">
-                Video Editing{" "}
+                Video Editing{' '}
               </Typography>
               <VideoCatalogue />
               <Banner />
